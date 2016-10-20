7d94d5ed05ece389cf1acb9cafae22e1b4b153bd
rpudlo <rpudlo@amtoss.com.ua>
2016-10-20 11:22:46 +0300

    Remove unused pages. Create jQuery property page.

--- src/InventoryManager.WebApplication/wwwroot/js/site.js
+++ src/InventoryManager.WebApplication/wwwroot/js/site.js
@@ -1,1 +1,43 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function () {
+    createTable();
+});
+
+function createTable() {
+    $.ajax({
+        url: 'http://localhost:59359/api/Property/',
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        success: function (properties) {
+            if ($("table").length == 0) {
+                $("body").append("<table></table>");
+            }
+            $.each(properties, function (index, property) {
+                $("table").append(buildRow(property));
+            });
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+}
+
+function buildRow(property) {
+    var ret =
+      "<tr>" +
+       "<td>" + property.name + "</td>" +
+       "<td>" + property.dataType + "</td>" +
+       "<td>" + property.description + "</td>" +
+      "</tr>";
+    return ret;
+}
+
+function handleException(request, message, error) {
+    var msg = "";
+    msg += "Code: " + request.status + "\n";
+    msg += "Text: " + request.statusText + "\n";
+    if (request.responseJSON != null) {
+        msg += "Message" + request.responseJSON.Message + "\n";
+    }
+    alert(msg);
+}
