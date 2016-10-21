1499d7dbcb01a3fa9046683b6c9426837f717986
Yuriy Peresyolkov <peresyolkov+github@gmail.com>
2016-10-21 16:29:57 +0300

    Form some interaction creation in progress

--- src/InventoryManager.WebApplication/wwwroot/js/site.js
+++ src/InventoryManager.WebApplication/wwwroot/js/site.js
@@ -1,6 +1,39 @@
 ﻿$(document).ready(function () {
-    createTable("Element");
+    createTable("element");
+
+    $("#saveButton").on("click", function (e) {
+        e.preventDefault();
+        addRecord();
+    });
+
+
+    $('form').submit(function (event) {
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'name': $('input[name=name]').val(),
+            'description': $('input[name=description]').val(),
+            'datatype': $('input[name=datatype]').val()
+        };
+        // process the form
+        $.ajax({
+            type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url: 'http://localhost:59359/api/property/', // the url where we want to POST
+            data: formData, // our data object
+            dataType: 'json', // what type of data do we expect back from the server
+            encode: true
+        })
+            // using the done promise callback
+            .done(function (data) {
+                // log data to the console so we can see
+                console.log(data);
+                // here we will handle errors and validation messages
+            });
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
 });
+
 
 function createTable(tableName) {
     $.ajax({
